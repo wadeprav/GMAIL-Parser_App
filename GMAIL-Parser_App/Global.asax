8d35ce5add0d374341f2123e14b2f515526be0dc
wadeprav <62180131+wadeprav@users.noreply.github.com>
2021-01-23 15:11:53 +0530

    Add project files.

---
+++ GMAIL-Parser_App/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GMAIL_Parser_App.MvcApplication" Language="C#" %>
---
+++ GMAIL-Parser_App/Models/XML.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GMAIL_Parser_App.Models
+{
+    public class XML
+    {
+        public string @version { get; set; }
+        public string @encoding { get; set; }
+    }
+}
