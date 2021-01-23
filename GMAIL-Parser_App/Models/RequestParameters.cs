178c7d9fc75bfdb94f97189b9c8d241ccd4ae8bc
wadeprav <62180131+wadeprav@users.noreply.github.com>
2021-01-23 20:56:35 +0530

    classes

---
+++ GMAIL-Parser_App/Models/RequestParameters.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GMAIL_Parser_App.Models
+{
+    public class RequestParameters
+    {
+        public string scope { get; set; }
+        public string access_type{ get; set; }
+        public string included_granted_scopes{ get; set; }
+        public string response_type{ get; set; }
+        public string state{ get; set; }
+        public string redirect_uri{ get; set; }
+        public string client_id { get; set; }
+        public string code{ get; set; }
+        public string client_secret{ get; set; }
+        public string grant_type{ get; set; }
+    }
+}
---
+++ GMAIL-Parser_App/Models/Root.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GMAIL_Parser_App.Models
+{
+    public class Root
+    {
+        public XML xml { get; set; }
+        public Feed feed { get; set; }
+    }
+}
