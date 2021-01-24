56192c18489eb49b473cb58d8b4b22d3eb80762d
wadeprav <62180131+wadeprav@users.noreply.github.com>
2021-01-24 23:56:37 +0530

    get code in coding

---
+++ GMAIL-Parser_App/Controllers/GmailAPIController.cs
@@ -0,0 +1,64 @@
+﻿using GMAIL_Parser_App.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace GMAIL_Parser_App.Controllers
+{
+    public class GmailAPIController : Controller
+    {
+        // GET: GmailAPI
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Code(string state, string code, string scope)
+        {
+            string GoogleWebAppClientID = WebConfigurationManager.AppSettings["GoogleWebAppClientID"];
+            string GoogleWebAppClientSecret = WebConfigurationManager.AppSettings["GoogleWebAppClientSecret"];
+            string RedirectUrl = WebConfigurationManager.AppSettings["RedirectUrl"];
+            string Token = CreateOAuthTokenForGmail(code, GoogleWebAppClientID, GoogleWebAppClientSecret, RedirectUrl);
+            Session["Token"] = Token;
+            return RedirectToAction("DisplayEmail");
+        }
+        public string CreateOAuthTokenForGmail(string code, string GoogleWebAppClientID, string GoogleWebAppClientSecret, string RedirectUrl)
+        {
+            RequestParameters requestParameters = new RequestParameters()
+            {
+                code = code,
+                client_id = WebConfigurationManager.AppSettings["GoogleWebAppClientID"],
+                client_secret = WebConfigurationManager.AppSettings["GoogleWebAppClientSecret"],
+                redirect_uri = WebConfigurationManager.AppSettings["RedirectUrl"],
+                grant_type = "authorization_code"
+            };
+            string inputJson = JsonConvert.SerializeObject(requestParameters);
+            string requestURI = "token";
+            string ResponseString = "";
+            HttpResponseMessage response;
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://oauth2.googleapis.com");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                StringContent content = new StringContent(inputJson, Encoding.UTF8, "application/json");
+                response = client.PostAsync(requestURI,content).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    ResponseString = JsonConvert.DeserializeObject(response.Content.ReadAsStringAsync().Result).ToString();
+                    var result = JsonConvert.DeserializeObject<OAuthTokenViewModel>(ResponseString);
+                    ResponseString = result.Access_token.ToString();
+                }
+            }
+            return ResponseString;
+        }
+    }
+}
